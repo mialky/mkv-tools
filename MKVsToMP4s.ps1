ed182e267a9dc4058a795ca61ce29e20f5314f51
mialky <mialky@outlook.com>
2022-11-21 11:16:57 +0200

    convert script

---
+++ MKVsToMP4s.ps1
@@ -0,0 +1,29 @@
+# ----------------------------------------------------------------------------
+# Converts (switches containers from) MKVs to MP4s.
+# ----------------------------------------------------------------------------
+
+# list entries (without extensions) as "mkv1_filename", "mkv2_filename", ...;
+# add single entries as ',<entry>':
+
+$entries = (
+	,
+)
+
+
+
+# validate input
+$confirm = Read-Host ("Starting '" + $entries[0] + ".mkv', is this correct? ('y' to confirm)")
+if (!($confirm -eq 'y')) {
+	Write-Host "Remember: single entries as ',<entry>'"
+	Return
+}
+
+Foreach ($item in $entries) {
+	Write-Host ("Processing '" + $item + ".mkv' ...")
+	Powershell ("ffmpeg -loglevel warning -hide_banner -stats -i '" + $item + ".mkv' -codec copy '" + $item + ".mp4'")
+	
+	# delete mkv
+	Powershell  ("rm '" + $item + ".mkv'")
+}
+
+Write-Host "Complete!"
